206667f12b74abb32dc2e11c8651dbde4a02418f
Hyunstone <hyun104467@naver.com>
2023-07-28 00:47:19 +0900

    feat: 시작 날짜에 따른 상태 등록(#2)

--- src/main/java/com/mogak/spring/domain/common/State.java
+++ src/main/java/com/mogak/spring/domain/common/State.java
@@ -1,5 +1,15 @@
 package com.mogak.spring.domain.common;
 
+import java.time.LocalDate;
+
 public enum State {
-    BEFORE,ONGOING,COMPLETE
+    BEFORE,ONGOING,COMPLETE;
+
+    public static State registerState(LocalDate startDate, LocalDate now) {
+        if (startDate.isEqual(now)) {
+            return ONGOING;
+        } else if (startDate.isAfter(now)) {
+            return BEFORE;
+        } else throw new RuntimeException("잘못 입력된 시작 날짜입니다.");
+    }
 }
