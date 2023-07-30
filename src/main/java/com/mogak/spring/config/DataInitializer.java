d5cfed0d61b10e4c7b6a09d45aede92a950a279f
Hyunstone <hyun104467@naver.com>
2023-07-30 17:20:00 +0900

    refactor: 데이터 insert 개선(#4)

--- src/main/java/com/mogak/spring/config/DataInitializer.java
+++ src/main/java/com/mogak/spring/config/DataInitializer.java
@@ -10,6 +10,8 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @AllArgsConstructor
@@ -23,34 +25,19 @@ public class DataInitializer implements ApplicationRunner {
     @Override
     public void run(ApplicationArguments args) throws Exception {
         insertMogakCategory();
+        insertAddress();
     }
 
     private void insertMogakCategory() {
-        MogakCategory certification = DataInitializer.getCategory("자격증");
-        MogakCategory externalActivities = DataInitializer.getCategory("대외활동");
-        MogakCategory exercise = DataInitializer.getCategory("운동");
-        MogakCategory insight = DataInitializer.getCategory("인사이트");
-        MogakCategory contest = DataInitializer.getCategory("공모전");
-        MogakCategory jobStudy = DataInitializer.getCategory("직무공부");
-        MogakCategory industryAnalysis = DataInitializer.getCategory("산업분석");
-        MogakCategory language = DataInitializer.getCategory("어학");
-        MogakCategory lectures = DataInitializer.getCategory("강연,강의");
-        MogakCategory project = DataInitializer.getCategory("프로젝트");
-        MogakCategory study = DataInitializer.getCategory("스터디");
-        MogakCategory etc = DataInitializer.getCategory("기타");
+        String[] taskArr = {
+                "자격증", "대외활동", "운동", "인사이트", "공모전", "직무공부",
+                "산업분석", "어학", "강연,강의", "프로젝트", "스터디", "기타"
+        };
 
-        mogakCategoryRepository.save(certification);
-        mogakCategoryRepository.save(externalActivities);
-        mogakCategoryRepository.save(exercise);
-        mogakCategoryRepository.save(insight);
-        mogakCategoryRepository.save(contest);
-        mogakCategoryRepository.save(jobStudy);
-        mogakCategoryRepository.save(industryAnalysis);
-        mogakCategoryRepository.save(language);
-        mogakCategoryRepository.save(lectures);
-        mogakCategoryRepository.save(project);
-        mogakCategoryRepository.save(study);
-        mogakCategoryRepository.save(etc);
+        for (String task: taskArr) {
+            MogakCategory categoryEntity = DataInitializer.getCategory(task);
+            mogakCategoryRepository.save(categoryEntity);
+        }
     }
 
     protected static MogakCategory getCategory(String task) {
@@ -60,39 +47,16 @@ public class DataInitializer implements ApplicationRunner {
     }
 
     private void insertAddress() {
-        Address seoul = getAddress("서울특별시");
-        Address sejong = getAddress("세종특별자치시");
-        Address daejeon = getAddress("대전광역시");
-        Address gwangju = getAddress("광주광역시");
-        Address daegu = getAddress("대구광역시");
-        Address busan = getAddress("부산광역시");
-        Address ulsan = getAddress("울산광역시");
-        Address gyeongnam = getAddress("경상남도");
-        Address gyeongbuk = getAddress("경상북도");
-        Address jeonnam = getAddress("전라남도");
-        Address jeonbuk = getAddress("전라북도");
-        Address chungnam = getAddress("충청남도");
-        Address chungbuk = getAddress("충청북도");
-        Address gangwon = getAddress("강원도");
-        Address jeju = getAddress("제주도");
-        Address dokdoAndUlleungdo = getAddress("독도/울릉도");
+        String[] addressArr = {
+                "서울특별시", "세종특별자치시", "대전광역시", "광주광역시", "대구광역시",
+                "부산광역시", "울산광역시", "경상남도", "경상북도", "전라남도", "전라북도",
+                "충청남도", "충청북도", "강원도", "제주도", "독도/울릉도"
+        };
 
-        addressRepository.save(seoul);
-        addressRepository.save(sejong);
-        addressRepository.save(daejeon);
-        addressRepository.save(gwangju);
-        addressRepository.save(daegu);
-        addressRepository.save(busan);
-        addressRepository.save(ulsan);
-        addressRepository.save(gyeongnam);
-        addressRepository.save(gyeongbuk);
-        addressRepository.save(jeonnam);
-        addressRepository.save(jeonbuk);
-        addressRepository.save(chungnam);
-        addressRepository.save(chungbuk);
-        addressRepository.save(gangwon);
-        addressRepository.save(jeju);
-        addressRepository.save(dokdoAndUlleungdo);
+        for (String address: addressArr) {
+            Address addressEntity = getAddress(address);
+            addressRepository.save(addressEntity);
+        }
     }
 
     private static Address getAddress(String address) {
