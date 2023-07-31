7e12022324747fccd78a6d12bffa0680871b0a43
Hyunstone <hyun104467@naver.com>
2023-07-31 23:29:49 +0900

    feat: createDto

--- src/main/java/com/mogak/spring/web/dto/UserResponseDto.java
+++ src/main/java/com/mogak/spring/web/dto/UserResponseDto.java
@@ -1,4 +1,14 @@
 package com.mogak.spring.web.dto;
 
+import lombok.*;
+
 public class UserResponseDto {
+
+    @Getter
+    @Builder
+    @AllArgsConstructor(access = AccessLevel.PRIVATE)
+    @NoArgsConstructor(access = AccessLevel.PRIVATE)
+    public static class CreateDto {
+
+    }
 }
