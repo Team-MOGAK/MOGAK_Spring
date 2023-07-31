43884625022f1e89ef2d1e5d3427db253f40ac58
hyunseok <hyun104467@naver.com>
2023-08-01 00:04:01 +0900

    feat: 유저 주소, 직무 불러오기(#5)

--- src/main/java/com/mogak/spring/service/UserServiceImpl.java
+++ src/main/java/com/mogak/spring/service/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package com.mogak.spring.service;
 
 import com.mogak.spring.converter.UserConverter;
+import com.mogak.spring.domain.user.Address;
 import com.mogak.spring.domain.user.Job;
 import com.mogak.spring.domain.user.User;
 import com.mogak.spring.repository.AddressRepository;
@@ -18,9 +19,15 @@ public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
     private final JobRepository jobRepository;
     private final AddressRepository addressRepository;
+
+    @Transactional
     @Override
     public User create(UserRequestDto.CreateUserDto response) {
-        User user = userRepository.save(UserConverter.toUser(response));
+        Job job = jobRepository.findJobByName(response.getJob())
+                .orElseThrow(() -> new RuntimeException("존재하지 않는 직업입니다"));
+        Address address = addressRepository.findAddressByName(response.getAddress())
+                .orElseThrow(() -> new RuntimeException("존재하지 않는 지역입니다"));
+        User user = userRepository.save(UserConverter.toUser(response, job, address));
         return null;
     }
     @Override
