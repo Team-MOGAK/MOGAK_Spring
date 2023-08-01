5f634a1e922be4da3a360936be0d44a76e0655c0
Hyunstone <hyun104467@naver.com>
2023-08-01 12:55:06 +0900

    fix: 반환 형태 수정(#5)

--- src/main/java/com/mogak/spring/repository/UserRepository.java
+++ src/main/java/com/mogak/spring/repository/UserRepository.java
@@ -7,5 +7,5 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     // 닉네임으로 유저 조회
-    Optional<User> findUserByNickname(String nickname);
+    Optional<User> findOneByNickname(String nickname);
 }
