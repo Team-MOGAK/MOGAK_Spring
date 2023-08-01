b39a18531574d33a2637f0caccdc1e7aebe29932
Hyunstone <hyun104467@naver.com>
2023-08-01 13:39:17 +0900

    test: 유저 생성 테스트(#5)

--- src/test/java/com/mogak/spring/service/UserServiceImplTest.java
+++ src/test/java/com/mogak/spring/service/UserServiceImplTest.java
@@ -1,12 +1,21 @@
 package com.mogak.spring.service;
 
+import com.mogak.spring.converter.UserConverter;
+import com.mogak.spring.domain.user.Address;
+import com.mogak.spring.domain.user.Job;
+import com.mogak.spring.domain.user.User;
 import com.mogak.spring.util.Regex;
+import com.mogak.spring.web.dto.UserRequestDto;
 import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
 
 class UserServiceImplTest {
 
     @Test
+    @DisplayName("닉네임_입력_유효성_테스트")
     void 닉네임_유효성_테스트() {
         //given
         String nick1 = "hyun1234";
@@ -27,5 +36,32 @@ class UserServiceImplTest {
         softly.assertThat(res3).isFalse();
         softly.assertThat(rightRes).isTrue();
     }
+
+    @Test
+    @DisplayName("DTO를_통한_유저_생성_테스트")
+    void 유저_생성_테스트() {
+        //when
+        UserRequestDto.CreateUserDto response =
+                UserRequestDto.CreateUserDto.builder()
+                        .nickname("hyun1234!")
+                        .job("개발/데이터")
+                        .address("경기도")
+                        .email("hyun1234@naver.com")
+                        .build();
+
+        //given
+        Job job = Job.builder()
+                .name(response.getJob())
+                .build();
+        Address address = Address.builder()
+                .name(response.getAddress())
+                .build();
+
+        //then
+        assertThat(UserConverter.toUser(response, job, address))
+                .isInstanceOf(User.class);
+    }
+
+
 
 }
