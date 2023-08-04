c932fbd52cbc2bea2f49dec9d2848ffa5c7cb6cd
Hyunstone <hyun104467@naver.com>
2023-08-04 13:51:25 +0900

    feat: 모각 페이징 조회 구현(#15)

--- src/main/java/com/mogak/spring/web/dto/MogakResponseDto.java
+++ src/main/java/com/mogak/spring/web/dto/MogakResponseDto.java
@@ -1,8 +1,13 @@
 package com.mogak.spring.web.dto;
 
+import com.mogak.spring.domain.mogak.Mogak;
+import com.mogak.spring.domain.mogak.MogakCategory;
+import com.mogak.spring.domain.mogak.MogakPeriod;
 import lombok.*;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class MogakResponseDto {
 
@@ -22,5 +27,28 @@ public class MogakResponseDto {
     public static class updateStateDto {
         private Long mogakId;
         private LocalDateTime updatedAt;
+    }
+
+    @Builder
+    @Getter
+    @AllArgsConstructor(access = AccessLevel.PROTECTED)
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    public static class getMogakListDto {
+        private List<getMogakDto> mogaks;
+        private int size;
+    }
+
+    @Builder
+    @Getter
+    @AllArgsConstructor(access = AccessLevel.PROTECTED)
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    public static class getMogakDto {
+        private String title;
+        private String state;
+        private List<String> periods;
+        private MogakCategory mogakCategory;
+        private String otherCategory;
+        private LocalDate startAt;
+        private LocalDate endAt;
     }
 }
