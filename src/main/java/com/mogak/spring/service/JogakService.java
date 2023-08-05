5edae030702fa229eed073f1afd9653f87159458
Hyunstone <hyun104467@naver.com>
2023-08-05 18:37:51 +0900

    feat: 조각 생성(#21)

--- src/main/java/com/mogak/spring/service/JogakService.java
+++ src/main/java/com/mogak/spring/service/JogakService.java
@@ -1,4 +1,8 @@
 package com.mogak.spring.service;
 
+import com.mogak.spring.domain.jogak.Jogak;
+import com.mogak.spring.web.dto.JogakRequestDto;
+
 public interface JogakService {
+    Jogak createJogak(JogakRequestDto.CreateJogakDto request);
 }
--- src/main/java/com/mogak/spring/web/controller/JogakController.java
+++ src/main/java/com/mogak/spring/web/controller/JogakController.java
@@ -1,6 +1,14 @@
 package com.mogak.spring.web.controller;
 
+import com.mogak.spring.converter.JogakConverter;
+import com.mogak.spring.domain.jogak.Jogak;
+import com.mogak.spring.service.JogakService;
+import com.mogak.spring.web.dto.JogakRequestDto;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +16,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/mogaks/jogaks")
 public class JogakController {
-    private final
+    private final JogakService jogakService;
+
+    @PostMapping("")
+    public ResponseEntity<Object> create(@RequestBody JogakRequestDto.CreateJogakDto request) {
+        Jogak jogak = jogakService.createJogak(request);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(JogakConverter.toJogakResponseDto(jogak));
+    }
 }
