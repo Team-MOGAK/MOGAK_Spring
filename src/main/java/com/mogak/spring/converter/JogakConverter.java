46a1ba7825ce2359f3156a72e73912fb050c98f1
Hyunstone <hyun104467@naver.com>
2023-08-07 15:23:59 +0900

    feat: 테스트를 위한 임시 조각 생성 api

--- src/main/java/com/mogak/spring/converter/JogakConverter.java
+++ src/main/java/com/mogak/spring/converter/JogakConverter.java
@@ -2,6 +2,7 @@ package com.mogak.spring.converter;
 
 import com.mogak.spring.domain.jogak.Jogak;
 import com.mogak.spring.domain.mogak.Mogak;
+import com.mogak.spring.web.dto.JogakResponseDto;
 
 public class JogakConverter {
     public static Jogak toJogak(Mogak mogak) {
@@ -9,10 +10,10 @@ public class JogakConverter {
                 .mogak(mogak)
                 .build();
     }
-//    public static JogakResponseDto.JogakDto toJogakResponseDto(Jogak jogak) {
-//        return JogakResponseDto.JogakDto.builder()
-//                .startTime(jogak.getStartTime())
-//                .build();
-//    }
+    public static JogakResponseDto.JogakDto toJogakResponseDto(Jogak jogak) {
+        return JogakResponseDto.JogakDto.builder()
+                .startTime(jogak.getStartTime())
+                .build();
+    }
 
 }
--- src/main/java/com/mogak/spring/web/dto/JogakResponseDto.java
+++ src/main/java/com/mogak/spring/web/dto/JogakResponseDto.java
@@ -5,12 +5,12 @@ import lombok.*;
 import java.time.LocalTime;
 
 public class JogakResponseDto {
-//    @Getter
-//    @Builder
-//    @AllArgsConstructor(access = AccessLevel.PRIVATE)
-//    @NoArgsConstructor(access = AccessLevel.PRIVATE)
-//    public static class JogakDto {
-//        private LocalTime startTime;
-//    }
+    @Getter
+    @Builder
+    @AllArgsConstructor(access = AccessLevel.PRIVATE)
+    @NoArgsConstructor(access = AccessLevel.PRIVATE)
+    public static class JogakDto {
+        private LocalTime startTime;
+    }
 
 }
