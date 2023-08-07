8d278bceb1227ff09632d60f9afb274968f5ed68
Hyunstone <hyun104467@naver.com>
2023-08-07 12:55:06 +0900

    test: 진행중인 오늘의 모각 불러오기(#25)

---
+++ src/test/java/com/mogak/spring/repository/MogakRepositoryTest.java
@@ -0,0 +1,55 @@
+package com.mogak.spring.repository;
+
+import com.mogak.spring.domain.common.State;
+import com.mogak.spring.domain.mogak.Mogak;
+import com.mogak.spring.service.MogakService;
+import com.mogak.spring.web.dto.MogakRequestDto;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.logging.Logger;
+
+import static org.assertj.core.api.SoftAssertions.*;
+
+@SpringBootTest
+class MogakRepositoryTest {
+
+    @Autowired
+    private MogakService mogakService;
+    @Autowired
+    private MogakRepository mogakRepository;
+
+    @Test
+    @DisplayName("진행중이며 오늘 해야하는 모각 불러오기")
+    void 해야하는_모각_불러오기_테스트() {
+        LocalDate today = LocalDate.now();
+        DayOfWeek dayOfWeek = today.getDayOfWeek();
+        int dayNum = dayOfWeek.getValue();
+
+        MogakRequestDto.CreateDto req1 =
+                MogakRequestDto.CreateDto.builder()
+                        .userId(1L)
+                        .title("스프링 해야딩")
+                        .category("직무공부")
+                        .days(List.of("MON", "TUE", "SAT", "SUN"))
+                        .startAt(LocalDate.now())
+                        .endAt(LocalDate.now().plusDays(7))
+                        .build();
+        mogakService.create(req1);
+
+        List<Mogak> mogaks =  mogakRepository.findAllOngoingToday(State.ONGOING.toString(), dayNum);
+        SoftAssertions softly = new SoftAssertions();
+        softly.assertThat(mogaks.size()).isEqualTo(1);
+
+
+    }
+
+
+}
