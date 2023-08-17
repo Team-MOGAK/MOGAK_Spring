b856f866b1e0f495a2b77d441e0001dd1521df1d
Ojimin <ogm0205@gmail.com>
2023-08-17 13:40:39 +0900

    [chore] : 배포코드커밋

--- src/main/java/com/mogak/spring/Application.java
+++ src/main/java/com/mogak/spring/Application.java
@@ -10,10 +10,11 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 public class Application {
 	//추후 ec2에 올릴때는 주석처리. 현재 테스트환경에서 s3에 객체를 올리기 위한 것
+	/*
 	static {
 		System.setProperty("com.amazonaws.sdk.disableEc2Metadata", "true");
 	}
-
+	*/
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
