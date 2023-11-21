2cb35dfc00023e50797aec03709dfd06dbab73e8
hyunseok <hyun104467@naver.com>
2023-11-22 01:27:54 +0900

    [refactor]: 모다라트 업데이트 수정 (#87)

--- src/main/java/com/mogak/spring/domain/modarat/Modarat.java
+++ src/main/java/com/mogak/spring/domain/modarat/Modarat.java
@@ -5,6 +5,7 @@ import com.mogak.spring.global.BaseEntity;
 import lombok.*;
 
 import javax.persistence.*;
+import java.util.Optional;
 
 @Builder
 @Getter
@@ -29,8 +30,8 @@ public class Modarat extends BaseEntity {
     private String validation;
 
     public void update(String title, String color) {
-        this.title = title;
-        this.color = color;
+        Optional.ofNullable(title).ifPresent(t -> this.title = t);
+        Optional.ofNullable(color).ifPresent(c -> this.color = c);
     }
 
 }
