b4210c2e5027a3ca8f97fa4e7119c134305c87b6
Ojimin <ogm0205@gmail.com>
2023-12-14 01:22:33 +0900

    [Fixt] : timezone 수정

--- src/main/java/com/mogak/spring/Application.java
+++ src/main/java/com/mogak/spring/Application.java
@@ -5,17 +5,29 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @EnableScheduling
 @EnableJpaAuditing
 @SpringBootApplication
 public class Application {
-	//추후 ec2에 올릴때는 주석처리. 현재 테스트환경에서 s3에 객체를 올리기 위한 것
+    //추후 ec2에 올릴때는 주석처리. 현재 테스트환경에서 s3에 객체를 올리기 위한 것
+    /*
+    static {
+        System.setProperty("com.amazonaws.sdk.disableEc2Metadata", "true");
+    }
+     */
 
-	static {
-		System.setProperty("com.amazonaws.sdk.disableEc2Metadata", "true");
-	}
+    /**
+     * 서버 timezone 설정
+     */
+    @PostConstruct
+    public void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
