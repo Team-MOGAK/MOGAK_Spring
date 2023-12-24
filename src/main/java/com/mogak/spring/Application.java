9453872b93418d35f0fa919e2d95b4f9aa05ac4d
Ojimin <ogm0205@gmail.com>
2023-12-24 14:55:18 +0900

    [Fix] ; build.gradle 수정

--- src/main/java/com/mogak/spring/Application.java
+++ src/main/java/com/mogak/spring/Application.java
@@ -27,8 +27,8 @@ public class Application {
 
 	//추후 ec2에 올릴때는 주석처리. 현재 테스트환경에서 s3에 객체를 올리기 위한 것
 
-//	static {
-//		System.setProperty("com.amazonaws.sdk.disableEc2Metadata", "true");
-//	}
+	static {
+		System.setProperty("com.amazonaws.sdk.disableEc2Metadata", "true");
+	}
 
 }
