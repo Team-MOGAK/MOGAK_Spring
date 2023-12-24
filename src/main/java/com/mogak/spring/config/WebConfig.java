8f3f603f3eb972f56df4fc88119c456515ece58b
Ojimin <ogm0205@gmail.com>
2023-12-24 22:24:13 +0900

    [Fix] : cors 오류 해결

--- src/main/java/com/mogak/spring/config/WebConfig.java
+++ src/main/java/com/mogak/spring/config/WebConfig.java
@@ -19,7 +19,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedHeaders("Authorization")
+                .allowedHeaders("Authorization","RefreshToken")
                 .allowedMethods(
                         HttpMethod.GET.name(),
                         HttpMethod.POST.name(),
