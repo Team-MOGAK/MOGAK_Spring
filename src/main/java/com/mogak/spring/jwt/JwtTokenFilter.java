3fbe35353d00dba273106b3df7d9dd1b23c43738
Ojimin <ogm0205@gmail.com>
2023-12-27 05:38:00 +0900

    [chore] : 수정중이다

--- src/main/java/com/mogak/spring/jwt/JwtTokenFilter.java
+++ src/main/java/com/mogak/spring/jwt/JwtTokenFilter.java
@@ -3,13 +3,11 @@ package com.mogak.spring.jwt;
 import com.mogak.spring.exception.AuthException;
 import com.mogak.spring.global.ErrorCode;
 import com.mogak.spring.redis.RedisService;
-import feign.Request;
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureException;
 import io.jsonwebtoken.UnsupportedJwtException;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.log4j.Log4j;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -29,7 +27,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Arrays;
-import java.util.List;
 
 
 @Component
@@ -40,31 +37,30 @@ public class JwtTokenFilter extends OncePerRequestFilter {
     private final JwtTokenProvider jwtTokenProvider;
     private final RedisService redisService;
     private final UserDetailsService userDetailsService;
-    @Value("${jwt.secret}")
-    private String secretKey;
 //    private static final List<String> EXCLUDE_URLS= Arrays.asList("/swagger-ui/index.html","/api/auth/login","/api/auth/refresh","/api/auth/logout","/api/users/nickname/verify","/api/users/join");
 
     @Override
     protected void doFilterInternal(HttpServletRequest servletRequest, HttpServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest request = (HttpServletRequest) servletRequest;
         HttpServletResponse response = (HttpServletResponse) servletResponse;
-//        String accessToken = jwtTokenProvider.resolveAccessToken(request);
-        String accessToken = resolveAccessToken(request);
+        log.info("현재 request: " + request);
+        String accessToken = jwtTokenProvider.resolveAccessToken(request);
         log.info("현재 accesstoken: " + accessToken);
+        System.out.print("현재 accesstoken: " + accessToken);
 
         try {
             log.info("현재 accesstoken: " + accessToken);
-//            if(accessToken==null){
-//                throw new AuthException(ErrorCode.EMPTY_TOKEN);
-//            }
-//            if(isLogout(accessToken)){ //로그아웃 검증
-//                throw new AuthException(ErrorCode.LOGOUT_TOKEN);
-//            }
-//            if(jwtTokenProvider.validateAccessToken(accessToken)){//access token 검증
-//                setAuthentication(accessToken); //검증된 토큰만 securitycontextholder에 토큰 등록
-//                log.info("인증 성공");
-//            }
-            setAuthentication(accessToken);
+            if(accessToken==null){
+                throw new AuthException(ErrorCode.EMPTY_TOKEN);
+            }
+            if(isLogout(accessToken)){ //로그아웃 검증
+                throw new AuthException(ErrorCode.LOGOUT_TOKEN);
+            }
+            if(jwtTokenProvider.validateAccessToken(accessToken)){//access token 검증
+                setAuthentication(accessToken); //검증된 토큰만 securitycontextholder에 토큰 등록
+                log.info("인증 성공");
+                System.out.print("인증성공");
+            }
         } catch (ExpiredJwtException e){//만료기간 체크
             throw new AuthException(ErrorCode.EXPIRE_TOKEN);
         } catch (SignatureException | UnsupportedJwtException | AuthException e){ //기존서명확인불가&jwt 구조 문제
@@ -73,35 +69,11 @@ public class JwtTokenFilter extends OncePerRequestFilter {
         filterChain.doFilter(request, response);
     }
 
-    //header에서 access token 가져오기
-    public String resolveAccessToken(HttpServletRequest request) {
-        if(request.getHeader("Authorization")!=null & request.getHeader("Authorization").startsWith("Bearer ")){
-            return request.getHeader("Authorization").substring(7);
-        }
-        return null;
-    }
-
-    //user email 검색
-    public String getUserPk(String token) {
-        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token)
-                .getBody().get("email").toString();
-    }
-
-    public Authentication getAuthentication(String token){
-        UserDetails userDetails = userDetailsService.loadUserByUsername(getUserPk(token));
-        if(userDetails == null){
-            throw new UsernameNotFoundException("User not found for useremail: " + getUserPk(token));
-        }
-        return new UsernamePasswordAuthenticationToken(userDetails,"",userDetails.getAuthorities());
-    }
-
-
     /**
      * Authentication 객체 생성
      */
     public void setAuthentication(String accessToken){
-//        Authentication authentication = jwtTokenProvider.getAuthentication(accessToken);
-        Authentication authentication = getAuthentication(accessToken);
+        Authentication authentication = jwtTokenProvider.getAuthentication(accessToken);
         SecurityContextHolder.getContext().setAuthentication(authentication);
     }
 
@@ -126,7 +98,6 @@ public class JwtTokenFilter extends OncePerRequestFilter {
                 "/api/auth/login","/api/auth/refresh","/api/auth/logout",
                 "/api/auth/withdraw", "/api/users/nickname/verify","/api/users/join"};
         String path=request.getRequestURI();
-        log.info("필터 처리에 제외된 uri 입니다");
         return Arrays.stream(excludePath).anyMatch(path::startsWith);
     }
 }
